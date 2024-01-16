33619dee52c8d3ec5d3c8885e6f51662b061b7c6
Yurii Vladyko <yuriivladyko@gmail.com>
2024-01-15 20:39:58 -0800

    Added Buttoms/Pic of cctb

--- test-project1.py
+++ test-project1.py
@@ -10,8 +10,22 @@ window.config(background='#EBEDE2')
 
 icon = PhotoImage(file='cctb.png')
 window.iconphoto(True, icon)
-
+img1 = icon.subsample(4, 4)
 #photo = PhotoImage(file='cctb.png')
+count = 0
+def submit():
+    user_ct = entry_ct.get()
+    print(f'Hello, {user_ct}')
+    entry_ct.config(state =DISABLED)
+def delete():
+    entry_ct.delete(0, END)
+def backspace():
+    entry_ct.delete(len(entry_ct.get())-1, END)
+
+def click():
+    global count
+    count +=1
+    print(f'You clicked the button {count} times')
 
 label_test = tk.Label(window,
                        text='Enter Your CCTB number :',
@@ -19,12 +33,38 @@ label_test = tk.Label(window,
                         font =('Arial', 30, 'bold'),
                         bd = 10,
                         relief = RAISED,
-                        padx = 20,
-                        pady = 20,
+                        #padx = 20,
+                       # pady = 20,
                         #image = photo,
                         #compound ='bottom'
                         )
+#button = tk.Button(window, 
+               # text= 'Search',
+               # command= click,
+               # font=('Comic Sans', 20),
+               # fg= '#00FF00',
+               # bg = 'black',
+               # activeforeground='#00FF00',
+               # activebackground='white',
+               # state = ACTIVE,
+               # image =icon,
+               # compound = 'top',)
+entry_ct = tk.Entry(window,
+                    font=('Arial', 20),
+                    fg='#00FF00',
+                    bg = 'white',
+                    )
+entry_ct.insert(0, 'CT')
+submit_btn = tk.Button(window, text ='Submit', command = submit)
+
+delete_btn = tk.Button(window, text ='Delete', command = delete,)
+
+backspace_btn = tk.Button(window, text ='Backspace', command = backspace,)
+
 label_test.pack()
+entry_ct.pack()
+#button.pack()
+delete_btn.pack(), backspace_btn.pack(), submit_btn.pack()
 #label_test.place(x= 0, y = 0)
 window.mainloop()
 
